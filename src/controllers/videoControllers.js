a81ea7174638d9cd5b6375fe10e8ccb70c0838cf
BKkom <79674766+bakkomcoder@users.noreply.github.com>
2023-05-02 23:59:40 +0900

    Connecting to Mongo

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,5 +1,4 @@
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Title" });
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
